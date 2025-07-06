0692d83eba308542ed1934b3381d315f5c81e2d2
ushel <utkarsh.shelke03@gmail.com>
2025-07-06 15:26:56 +0530

    stage_02_tansfer learning compeleted

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,4 +1,5 @@
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import get_VGG_16_model
 import argparse 
 import pandas as pd  
 import os   
@@ -11,23 +12,38 @@ logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
 # create_directory([log_dir]) # not storing logs so use os.makdirs
 os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level = logging.INFO, format=logging_str, filemode='a')
-
-def copy_file(source_download_dir,local_data_dir):
-    list_of_files = os.listdir(source_download_dir)
-    N = len(list_of_files)
-    for file in tqdm(list_of_files, total=N, desc=f"copying file from {source_download_dir} to {local_data_dir}", colour="green"):
-        src = os.path.join(source_download_dir,file)
-        dest = os.path.join(local_data_dir,file)
-        shutil.copy(src, dest)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level = logging.INFO, format=logging_str, filemode='a')  
     
-    
-def prepare_base_model(config_path):
+def prepare_base_model(config_path,params_path):
     config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    # 2. Use transfer learning
+    artifacts = config["artifacts"]
+    artifacts_dir= artifacts["ARTIFACTS_DIR"]
     
-    source_download_dirs = config["source_download_dirs"]
-    local_data_dirs = config["local_data_dirs"]
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
+    base_model_name = artifacts["BASE_MODEL_NAME"]
+    base_model_dir_path = os.path.join(artifacts_dir,base_model_dir)
     
+    create_directory([base_model_dir_path])
+    
+    base_model_path = os.path.join(base_model_dir_path,base_model_name)
+    model  = prepare_model(
+                model,
+                CLASSES=params["CLASSES"],
+                freeze_all=True,
+                freeze_till=None,
+                learning_rate=params["LEARNING_RATE"]
+                           )
+    
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path = base_model_path)
+    #transfer learning means we will only remove fully connected layers and will keep all the convolutional layers whihci is trained on imagenet dataset, also fully connected layers has 1000 classes we only required 2 classes.
+    
+    # 3. Prepare the callbacks tensorboard logs or checkpointing
+    
+    # 4. Training module(take data from get data section, prepare custom model and take all the call back ---> trained model (file required  - config.yaml, secrets.yaml, params.yaml, artifacts directory,dvc.yaml,loogs.yaml, setup.py))
+    # artifacts folder --> model, callbacks binary 
+    # logs --> General logs and tensorboard logs 
     for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs), total=2, desc ="list of folders",colour = "red"):
         #create folder to copy the data
         create_directory([local_data_dir])
@@ -39,12 +55,15 @@ if __name__ == '__main__':
     
     args.add_argument("--config","-c",default="config/config.yaml")
     
+    args.add_argument("--params","-p",default="params.yaml")
+
+    
     parsed_args = args.parse_args()
     
     try:
         # print_fn("test") # to check exception
         logging.info("\n>>>>>>>>>>>>>>>>stage two started")
-        prepare_base_model(config_path=parsed_args.config)
+        prepare_base_model(config_path=parsed_args.config, params_path= parsed_args.params)
         logging.info("stage two completed! base model is created>>>>>>>>>>>>>>>>>>>>")
         
     except Exception as e:
---
+++ src/utils/models.py
@@ -0,0 +1,32 @@
+import tensorflow as tf  
+import os  
+import joblib   
+import logging
+
+
+def get_VGG_16_model(input_shape,model_path):
+    model = tf.keras.applications.vgg16.VGG16(
+        input_shape=input_shape,
+        weights="imagenet",
+        include_top=False  # getting rid of fully connected layer of VGG16 model we just keeping onvolution layer which is trained on imagenet dataset
+    )
+    
+    model.save(model_path)
+    logging.info(f"VGG16 model saved at {model_path}")
+    return model
+
+def prepare_model(model, CLASSES, freeze_all, freeze_till, learning_rate):
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable = False 
+    elif(freeze_till is not None) and (freeze_till > 0):
+        for layer in model.layers[:freeze_till]:
+            layer.trainable = False 
+            
+    # add our own fully connected layers... 
+    # 7 X 7 X 512 to single array it can be process as an input layer for fully connected layer
+    flatten_in = tf.keras.layers.Flatten()(model.output)
+    prediction = tf.keras.layers.Dense( #as tis is other layer need to connect with flatten so think it as function
+        units = CLASSES,
+        activation="softmax"
+    )(flatten_in) # functional approach
