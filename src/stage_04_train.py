0d2b45d8b142427b91191e3ee142ff66df99eb93
ushel <utkarsh.shelke03@gmail.com>
2025-07-07 16:09:23 +0530

    data dir added stage_04 train and validation generator created

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -2,6 +2,7 @@
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.models import load_full_model
 from src.utils.callbacks import get_callbacks
+from src.utils.data_management import train_valid_generator
 import argparse 
 import pandas as pd  
 import os   
@@ -34,7 +35,12 @@ def train_model(config_path, params_path):
     callback_dir_path = os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
     callbacks = get_callbacks(callback_dir_path)
     
-    
+    train_generator, valid_generator = train_valid_generator(
+        data_dir = artifacts["DATA_DIR"],
+        IMAGE_SIZE = (params["IMAGE_SIZE"][::-1]),
+        BATCH_SIZE = params["BATCH_SIZE"],
+        do_data_augmentation=params["AUGMENTATION"]
+    )
     
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
