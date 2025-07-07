7da527b6ab449dd4f7c65949560fd0d3a696da8a
ushel <utkarsh.shelke03@gmail.com>
2025-07-07 15:38:55 +0530

    stage 04 model and callbacks read back

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,5 +1,7 @@
         
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import load_full_model
+from src.utils.callbacks import get_callbacks
 import argparse 
 import pandas as pd  
 import os   
@@ -21,12 +23,23 @@ def train_model(config_path, params_path):
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
     
+    train_model_dir_path = os.path.join(artifacts_dir,artifacts["TRAINED_MODEL_DIR"])
+    create_directory([train_model_dir_path])
+    
+    untrained_full_model_path = os.path.join(artifacts_dir, artifacts["BASE_MODEL_DIR"],
+                                             artifacts["UPDATED_BASE_MODEL_NAME"])
+    
+    model = load_full_model(untrained_full_model_path)
+    
+    callback_dir_path = os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
+    callbacks = get_callbacks(callback_dir_path)
+    
+    
     
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     
     args.add_argument("--config","-c",default="config/config.yaml")
-    
     args.add_argument("--params","-p",default="params.yaml")
 
     
@@ -36,7 +49,7 @@ if __name__ == '__main__':
         # print_fn("test") # to check exception
         logging.info("\n>>>>>>>>>>>>>>>>stage Four started")
         train_model(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage Four completed! training completed and model is saved >>>>>>>>>>>>>>>>>>>>")
+        logging.info("stage Four completed! training completed and model is saved>>>>>>>>>>>>>>>>>>>>\n\n")
         
     except Exception as e:
         logging.exception(e)
